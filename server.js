0dd1c0dc13b2804e50b6bf4e3371d3dc5304528a
x212796 <aftab.ahmed@telus.com>
2019-09-02 00:02:08 +0530

    nodejs based restful apis

--- server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app = express();
-app.get("/", (req, res, next) => {
-    res.send("TourBooker API");
-});
-app.listen(process.env.PORT || 8091, () => console.log("Server Started..."));
--- server.ts
+++ server.ts
@@ -1,17 +1,29 @@
-import * as express from "express";
+import express from "express";
 const app = express();
 
+import * as bodyparser from "body-parser";
+const jsonParser = bodyparser.json();
+
+import { DataStore } from "./data/data";
+import { apiGetTours } from "./api/tours/apiGetTours";
+import { apiGetTourDetail } from "./api/tours/apiGetTourDetail";
+import { apiCreateTour } from "./api/tours/apiCreateTour";
+import { apiDeleteTour } from "./api/tours/apiDeleteTour";
+import { apiUpdateTour } from "./api/tours/apiUpdateTour";
 
 app.get("/", (req, res, next) => {
     res.send("TourBooking API");
 });
 
-app.get("/tours", (req, res, next) => {
-    res.send("Get a list of tours");
-});
+app.get("/tours", apiGetTours);
 
-app.post("/tours", (req, res, next) => {
-    res.send("Post a new tour");
-});
+app.get("/tours/:id", apiGetTourDetail);
+
+app.post("/tours", jsonParser, apiCreateTour);
+
+app.delete("/tours/:id", apiDeleteTour);
+
+app.patch("/tours/:id", jsonParser, apiUpdateTour);
+
 
 app.listen(process.env.PORT || 8091, () => console.log("Server Started..."));
